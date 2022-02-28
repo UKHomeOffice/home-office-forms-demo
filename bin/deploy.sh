6e0f73e0437d07400924eae9b2760eb29f0ab1d4
Alexander Swann <alexander.swann@digital.homeoffice.gov.uk>
2022-02-28 14:18:06 +0000

    Setup initial DB file to test integration with RDS

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -1,12 +1,15 @@
 #! /bin/bash
 set -e
 
+export SCHEMA_ACTION=migrate
+
 kd='kd --insecure-skip-tls-verify --timeout 10m --check-interval 10s'
 
 if [[ $1 == 'tear_down' ]]; then
   export KUBE_NAMESPACE=$BRANCH_ENV
   export DRONE_SOURCE_BRANCH=$(cat /root/.dockersock/branch_name.txt)
 
+  $kd --delete -f kube/jobs/ms-schema-job.yml
   $kd --delete -f kube/configmaps/configmap.yml
   $kd --delete -f kube/redis -f kube/app
   echo "Torn Down UAT Branch - $APP_NAME-$DRONE_SOURCE_BRANCH.$BRANCH_ENV.homeoffice.gov.uk"
@@ -17,9 +20,13 @@ export KUBE_NAMESPACE=$1
 export DRONE_SOURCE_BRANCH=$(echo $DRONE_SOURCE_BRANCH | tr '[:upper:]' '[:lower:]' | tr '/' '-')
 
 if [[ ${KUBE_NAMESPACE} == ${BRANCH_ENV} ]]; then
+  $kd --delete -f kube/jobs/ms-schema-job.yml
+  $kd -f kube/jobs/ms-schema-job.yml
   $kd -f kube/configmaps -f kube/certs
   $kd -f kube/redis -f kube/app
 elif [[ ${KUBE_NAMESPACE} == ${PROD_ENV} ]]; then
+  $kd --delete -f kube/jobs/ms-schema-job.yml
+  $kd -f kube/jobs/ms-schema-job.yml
   $kd -f kube/configmaps
   $kd -f kube/redis -f kube/app
 fi
