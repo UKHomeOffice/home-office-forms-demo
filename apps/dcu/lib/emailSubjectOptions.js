231765afb1ad39c7db1f3544c75911fcf6a48d74
Robert McCann <robert.mccann@digital.homeoffice.gov.uk>
2022-08-11 12:33:46 +0100

    add dcu prototype

---
+++ apps/dcu/lib/emailSubjectOptions.js
@@ -0,0 +1,20 @@
+'use strict';
+
+module.exports = {
+  getEmailSubjectOptions: function () {
+    return [
+    {
+      value: 'O1',
+      label: 'Option 1 - How are you?'
+    },
+    {
+      value: '02',
+      label: 'Option 2 - Why'
+    },
+    {
+      value: '03',
+      label: 'Option 3 - Hello'
+    }
+  ];
+  }
+};
