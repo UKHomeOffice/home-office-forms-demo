6ee231941797ed039d209e4a4925edc6f2301307
Robert McCann <robert.mccann@digital.homeoffice.gov.uk>
2022-08-11 15:28:15 +0100

    add confirm

--- apps/dcu/sections/summary-data-sections.js
+++ apps/dcu/sections/summary-data-sections.js
@@ -2,35 +2,26 @@
 
 const moment = require('moment');
 const PRETTY_DATE_FORMAT = 'Do MMMM YYYY';
+const _ = require('lodash');
+const SUBJECT_OPTIONS = require('../lib/emailSubjectOptions').getEmailSubjectOptions();
 
 module.exports = {
   applicantsDetails: [
-    'name',
+    'name', 'email',
     {
       field: 'dateOfBirth',
       parse: d => d && moment(d).format(PRETTY_DATE_FORMAT)
     }
-  ],
-  address: [
+  ], address: [
     'building',
     'street',
     'townOrCity',
     'postcode'
-  ],
-  income: [
-    'incomeTypes'
-  ],
-  contactDetails: [
-    'email',
-    'phone'
-  ],
-  countrySelect: [
-    'countrySelect'
-  ],
-  complaintDetails: [
-    'complaintDetails'
-  ],
-  weaponsTypes: [
-    'weaponsTypes'
+  ], emailQuery: [
+    {
+      field: 'emailSubject',
+      parse: v => _.get(_.find(SUBJECT_OPTIONS, stage => stage.value === v), 'label', '')
+    },
+    'description'
   ]
 };
