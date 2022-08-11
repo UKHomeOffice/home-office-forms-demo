91f832321ee682c5b4fdec20fb6544e060446a9e
Rhodine Orleans-Lindsay <43888758+Rhodine-orleans-lindsay@users.noreply.github.com>
2022-08-11 14:28:10 +0100

    fix liniting errors

--- apps/dcu/lib/emailSubjectOptions.js
+++ apps/dcu/lib/emailSubjectOptions.js
@@ -3,18 +3,18 @@
 module.exports = {
   getEmailSubjectOptions: function () {
     return [
-    {
-      value: 'O1',
-      label: 'Option 1 - How are you?'
-    },
-    {
-      value: '02',
-      label: 'Option 2 - Why'
-    },
-    {
-      value: '03',
-      label: 'Option 3 - Hello'
-    }
-  ];
+      {
+        value: 'O1',
+        label: 'Option 1 - How are you?'
+      },
+      {
+        value: '02',
+        label: 'Option 2 - Why'
+      },
+      {
+        value: '03',
+        label: 'Option 3 - Hello'
+      }
+    ];
   }
 };
