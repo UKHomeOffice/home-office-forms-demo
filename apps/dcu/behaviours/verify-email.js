ee23651532e93bf56379f82c378d69d2d6dabe9d
Robert McCann <robert.mccann@digital.homeoffice.gov.uk>
2022-08-11 12:50:11 +0100

    add rhodines changes

---
+++ apps/dcu/behaviours/verify-email.js
@@ -0,0 +1,23 @@
+// 'use strict';
+
+module.exports = SuperClass => class extends SuperClass {
+  validate(req, res, next) {
+    if (req.form.values['email'] !== req.form.values['email-verify']) {
+      return next({
+        'email-verify': new this.ValidationError(
+          'email-verify',
+          {
+            type: 'notSame'
+          }
+        ),
+        'email': new this.ValidationError(
+          'email',
+          {
+            type: 'notSame'
+          }
+        )
+      });
+    } super.validate(req, res, next);
+    return next;
+  }
+}; 
