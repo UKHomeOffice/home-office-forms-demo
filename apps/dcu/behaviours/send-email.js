f2e41b8403e64504a454137aec22872559b39617
Robert McCann <robert.mccann@digital.homeoffice.gov.uk>
2022-08-12 14:20:15 +0100

    fix email subject

--- apps/dcu/behaviours/send-email.js
+++ apps/dcu/behaviours/send-email.js
@@ -1,12 +1,18 @@
 'use strict';
-
+const emailSubjectOptions = require('../fields.js')['emailSubject'].options;
 const utils = require('../lib/utils');
 const uuid = require('uuid');
+const _ = require('lodash');
+
 
 module.exports = superclass => class Submit extends superclass {
   saveValues(req, res, next) {
     const reference = uuid.v1();
-    return utils.sendEmail(req.sessionModel.toJSON(), reference)
+    const emailSubjectValue = req.form.historicalValues['emailSubject'];
+    const emailSubject = _.get( _.find(emailSubjectOptions, e => e.value === emailSubjectValue), 'label', '');
+
+
+    return utils.sendEmail(req.sessionModel.toJSON(), reference, emailSubject)
       .then(() => Submit.handleSuccess(req, next, reference, true))
       .catch(err => Submit.handleError(req, next, reference, err, true));
   }
--- apps/dcu/lib/emailSubjectOptions.js
+++ apps/dcu/lib/emailSubjectOptions.js
@@ -4,23 +4,23 @@ module.exports = {
   getEmailSubjectOptions: function () {
     return [
       {
-        value: 'O1',
+        value: 'O1 - Problems with your visa application',
         label: 'Problems with your visa application'
       },
       {
-        value: '02',
+        value: '02 - Making a complaint about a member of staff',
         label: 'Making a complaint about a member of staff'
       },
       {
-        value: '03',
+        value: '03 - Reporting a crime in an overseas territory',
         label: 'Reporting a crime in an overseas territory'
       },
       {
-        value: '04',
+        value: '04 - Reporting counterfeit money',
         label: 'Reporting counterfeit money'
       },
       {
-        value: '05',
+        value: '05 - Other',
         label: 'Other'
       }
     ];
