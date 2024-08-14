0a734b1bdb816bb070a8a76075cf497a41a713eb
shamiluwais <mohamed.uwais@digital.homeoffice.gov.uk>
2024-08-14 17:17:09 +0100

    log the client ip address

---
+++ apps/demo/behaviours/log-ipaddress.js
@@ -0,0 +1,9 @@
+'use strict';
+
+module.exports = SuperClass => class extends SuperClass {
+  getValues(req, res, next) {
+    const ip = (req.headers['x-forwarded-for'] || req.connection.remoteAddress || '').split(',')[0].trim();
+    console.log('ipaddress', ip);
+    return super.getValues(req, res, next);
+  }
+};
